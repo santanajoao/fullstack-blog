d341d92ba7ba6816c32a4c4d13d0ca7bb10c935b
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-09 02:01:55 -0300

    adiciona seção de postagens em alta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,14 @@ const topics: Topics[] = [
   { name: 'Jogos', image: '/jogos.jpg', link: '' },
 ];
 
+const posts = [
+  { image: '/inteligencia-artificial.jpg', author: 'João Pedro', date: '08/06', title: 'A inteligência artificial vai te substituir?', description: 'Entenda o verdadeiro impacto do avanço das inteligências artificiais no seu trabalho e...', link: '' },
+  { image: '/esporte.jpg', author: 'Anderson Souza', date: '08/06', title: 'Quantas series fazer para hipertrofia?', description: 'Sempre ficou em dúvida em quantas seríes deve fazer? Sempre ouviu que cada caso é...', link: '' },
+  { image: '/progamacao.jpg', author: 'Arnaldo Teves', date: '07/06', title: 'Como eu aprendi javascript', description: 'Você sempre sonhou em ser programador, mas não sabe por onde começar? Nesse post...', link: '' },
+  { image: '/musica.jpg', author: 'Carla Lorena', date: '12/05', title: 'Músicas para você que quer explorar o mundo...', description: 'Gosta de rock e cansou de ouvir só as mesmas músicas? Aqui mostrare para você desde a...', link: '' },
+  { image: '/moda.jpg', author: 'Marcos Vinicius', date: '23/02', title: 'Combinações simples, mas estilosas para...', description: 'Se você é lowprofile provavelmente já teve dificuldade em variar suas...', link: '' },
+];
+
 export default function Home() {
   return (
     <>
@@ -59,7 +67,7 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="px-3 sm:px-5 py-4 sm:py-6">
+        <section className="px-3 sm:px-5 py-4 sm:py-6 space-y-5">
           <article>
             <h2 className="font-bold text-lg sm:text-2xl">
               Explore os tópicos mais falados
@@ -73,6 +81,27 @@ export default function Home() {
                     <h3 className="absolute font-bold text-center z-10 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
                       {name}
                     </h3>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </article>
+
+          <article>
+            <h2 className="font-bold text-lg sm:text-2xl">
+              Publicações em alta
+            </h2>
+
+            <ul className="flex space-x-5 mt-3 overflow-y-auto max-w-full hidden-scroll">
+              {posts.map(({ author, date, description, image, title, link }) => (
+                <li key={title} className="w-56 flex-shrink-0 overflow-hidden relative">
+                  <Link href={link} className="h-full w-full flex flex-col space-y-1">
+                    <Image src={image} alt={title} height={500} width={500} className="object-cover rounded-2xl"/>
+                    <span className="text-xs">{`${author} - ${date}`}</span>
+                    <h3 className="font-bold text-lg leading-5">
+                      {title}
+                    </h3>
+                    <p className="mt-1 text-justify">{description}</p>
                   </Link>
                 </li>
               ))}
