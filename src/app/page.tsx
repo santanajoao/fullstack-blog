b6dcb2682a8ec019ae488ad73f922895c69fb118
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-09 00:45:50 -0300

    cria hero da página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import { Noto_Serif as NotoSerif } from "next/font/google";
+
+const notoSerif = NotoSerif({ weight: ['400'], subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -18,7 +21,23 @@ export default function Home() {
           </Link>
         </div>
       </header>
-      <main>Home</main>
+      <main>
+        <section className="bg-[#82f880] py-24 px-3 sm:px-5 relative overflow-hidden">
+          <span className={`${notoSerif.className} block text-white uppercase text-5xl sm:text-6xl md:text-7xl text-right absolute right-0 top-1/2 -translate-y-1/2 blur-sm tracking-widest`}>
+            Escreva. Leia. Crie. Imagine. Compartilhe. Informe. Seja.
+          </span>
+
+          <div className="z-10 relative">
+            <h1 className={`${notoSerif.className} text-3xl sm:text-4xl max-w-lg mb-7`}>
+              Liberte sua criatividade. Compartilhe. Escreva.
+            </h1>
+            
+            <Link href="/write" className="bg-black text-white rounded-full px-3 py-2">
+              Começar a escrever
+            </Link>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
