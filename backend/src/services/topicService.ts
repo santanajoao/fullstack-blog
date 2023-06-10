b0c4bcb447c6e90e514d7c212317a4632d091381
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-09 21:43:28 -0300

    corige query e cria utilitário para datas

--- backend/src/services/topicService.ts
+++ backend/src/services/topicService.ts
@@ -1,16 +1,8 @@
 import { PrismaClient, Topic } from '@prisma/client';
 import { AsyncServiceResponse } from '../types/ServiceResponse';
+import { getDateDaysAgo } from '../utils/dates';
 
 const prisma = new PrismaClient();
-
-const getDateDaysAgo = (days: number): Date => {
-  const MS_PER_DAY = 86400000;
-  
-  const daysInMs = days * MS_PER_DAY;
-  const dateInMs = Date.now() - daysInMs;
-  const dateDaysAgo = new Date(dateInMs);
-  return dateDaysAgo;
-}
 
 const getPopularTopicIds = async (): AsyncServiceResponse<number[]> => {
   const dateSevenDaysAgo = getDateDaysAgo(7);
