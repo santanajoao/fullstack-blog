e77475b92fa3ff09219067257ceedddaa7b019a1
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-11 15:25:55 -0300

    instala e utiliza helmet para segurança da api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import helmet from 'helmet';
 import routers from './routers';
 
 const app = express();
+app.use(helmet())
 
 app.use('/topics', routers.topicRoutes);
 app.use('/posts', routers.postRoutes);
