b71c0af5a49b3465f9247becc647fe6789e22ed2
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-11 15:32:15 -0300

    instala o middleware cors e aplica com configurações iniciais

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import helmet from 'helmet';
 import routers from './routers';
+import cors from 'cors';
 
 const app = express();
-app.use(helmet())
+app.use(helmet());
+app.use(cors());
 
 app.use('/topics', routers.topicRoutes);
 app.use('/posts', routers.postRoutes);
