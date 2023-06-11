8c49783125f8b5959fd8926accc11a045a2a519c
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-11 18:41:12 -0300

    adiciona url do frontend no cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,11 @@ import cors from 'cors';
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+app.use(cors(corsOptions));
 
 app.use('/topics', routers.topicRoutes);
 app.use('/posts', routers.postRoutes);
