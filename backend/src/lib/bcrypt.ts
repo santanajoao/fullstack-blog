770546e3b7279342f32896cbef78c2bce0ec4b89
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-13 12:43:52 -0300

    cria utilitário de encriptagem e asserção das senhsa

---
+++ backend/src/lib/bcrypt.ts
@@ -0,0 +1,17 @@
+import bcrypt from 'bcrypt';
+
+const encrypt = async (data: string): Promise<string> => {
+  const SALT_ROUNDS = 10;
+  const hash = await bcrypt.hash(data, SALT_ROUNDS);
+  return hash;
+}
+
+const compare = async (hash: string, data: string) => {
+  const isEqual = await bcrypt.compare(data, hash);
+  return isEqual;
+};
+
+export default {
+  encrypt,
+  compare,
+};
