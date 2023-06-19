1ed177bb3ad98d6949db5b6336813bae377811f9
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-19 18:50:25 -0300

    cria testes de integração da rota GET /posts/popular

---
+++ backend/tests/mocks/post.mock.ts
@@ -0,0 +1,25 @@
+import { Post } from "@prisma/client";
+
+const post: Post = {
+  id: 'UUID',
+  createdAt: new Date(),
+  description: 'Description',
+  imageUrl: 'https://image-link.com',
+  likes: 1,
+  title: 'Title',
+  userId: 'UUID',
+};
+
+const postList: Post[] = [post];
+
+// na resposta json os valores são convertidos 
+const postListResponse = postList.map((post) => ({
+  ...post,
+  createdAt: post.createdAt.toISOString(),
+}));
+
+export default {
+  post,
+  postList,
+  postListResponse,
+};
---
+++ backend/tests/post.test.ts
@@ -0,0 +1,31 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import sinon from 'sinon';
+import prisma from '../src/lib/prisma';
+import app from '../src/app';
+import topicPostHelpers from '../src/services/helpers/topicPost.helpers';
+import postMock from './mocks/post.mock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Post routes integration tests', function () {
+  beforeEach(sinon.restore);
+
+  describe('GET /posts/popular', function () {
+    it('should return the post list', async function () {
+      sinon.stub(topicPostHelpers, 'getWeekTopicsPosts').resolves([]);
+      sinon.stub(prisma, 'post').value({
+        findMany: sinon.stub().resolves(postMock.postList),
+      });
+  
+      const { status, body } = await chai
+        .request(app)
+        .get('/posts/popular');
+    
+      expect(status).to.be.equal(200)
+      expect(body).to.be.deep.equal(postMock.postListResponse);
+    });
+  });
+});
