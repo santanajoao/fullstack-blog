f84687f68a396cb7b0f40e185dcff0ce7a5626c7
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-19 12:16:03 -0300

    cria utilitário de token jwt

---
+++ backend/src/lib/jwt.ts
@@ -0,0 +1,17 @@
+import * as jwt from 'jsonwebtoken';
+import { Payload } from '../types/jwt/Payload';
+
+const getSecret = (): string => {
+  const secret = process.env.JWT_SECRET;
+  if (!secret) {
+    throw new Error('Secret not found');
+  }
+  return secret;
+}
+
+export const createToken = (payload: Payload): string => {
+  const options = { expiresIn: '5d' };
+  const secret = getSecret();
+  const token = jwt.sign(payload, secret, options);
+  return token;
+};
