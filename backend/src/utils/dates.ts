12049b8cfafc1c6ec292e6fac68dfa6559667ce8
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-18 21:46:12 -0300

    atualiza seeders para possuírem datas na semana automáticas

--- backend/src/utils/dates.ts
+++ backend/src/utils/dates.ts
@@ -1,8 +1,16 @@
+const MS_PER_DAY = 86400000;
+
 export const getDateDaysAgo = (days: number): Date => {
-  const MS_PER_DAY = 86400000;
-  
   const daysInMs = days * MS_PER_DAY;
   const dateInMs = Date.now() - daysInMs;
   const dateDaysAgo = new Date(dateInMs);
   return dateDaysAgo;
+}
+
+export const getDateBetweenNowAnd = (daysAgo: number): Date => {
+  const randomDaysDiff = Math.floor(Math.random() * (daysAgo + 1));
+  const daysInMs = randomDaysDiff * MS_PER_DAY;
+  const dateInMs = Date.now() - daysInMs;
+  const randomDate = new Date(dateInMs);
+  return randomDate;
 }
