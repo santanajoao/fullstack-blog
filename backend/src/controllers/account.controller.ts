110d3fd5364dd71c990dbc516ce069deab4de13f
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-22 02:21:50 -0300

    cria controller de login

--- backend/src/controllers/account.controller.ts
+++ backend/src/controllers/account.controller.ts
@@ -14,6 +14,17 @@ const handlePostAccount = async (req: Request, res: Response) => {
   res.status(201).json(data);
 };
 
+const handlePostSignIn = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const { status, data } = await accountService.signIn(email, password);
+  if (status !== 'SUCCESS') {
+    return res.status(mapErrorStatus(status)).json(data);
+  }
+  res.status(200).json(data);
+};
+
 export default {
   handlePostAccount,
+  handlePostSignIn,
 };
