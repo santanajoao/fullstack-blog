b64d8b5705d24f5f4d5a556a7511ef8f678bd9ae
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-24 16:33:00 -0300

    cria schema de validação do formulário de login e criação

---
+++ frontend/src/lib/schemas/sign.schema.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+
+export const nameMinLength = 3;
+export const nameMaxLength = 30;
+export const passwordMinLength = 8;
+export const passwordMaxLength = 126;
+
+export const signNameSchema = z
+  .string()
+  .min(nameMinLength, {
+    message: `O nome deve ter pelo menos ${nameMinLength} caracteres`,
+  })
+  .max(nameMaxLength, {
+    message: `O nome deve ter ${nameMaxLength} caracteres ou menos`,
+  });
+
+export const signEmailSchema = z
+  .string()
+  .nonempty('O email é obrigatório')
+  .email('O email deve ser válido');
+
+export const signPasswordSchema = z
+  .string()
+  .min(passwordMinLength, `A senha deve ter pelo menos ${passwordMinLength} caracteres`)
+  .max(passwordMaxLength, `A senha deve ter ${passwordMaxLength} caracteres ou menos`);
+
+export const signUpSchema = z.object({
+  name: signNameSchema,
+  email: signEmailSchema,
+  password: signPasswordSchema,
+});
+
+export const signInSchema = z.object({
+  email: signEmailSchema,
+  password: signPasswordSchema,
+});
