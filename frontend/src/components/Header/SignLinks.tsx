6afb19c3d6a4f3a3aa12a55bc0aa4e307c3696d8
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-26 20:32:03 -0300

    cria componente para os links de sign

---
+++ frontend/src/components/Header/SignLinks.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+export default function SignLinks() {
+  return (
+    <div className="space-x-3">
+      <Link href="/signup" className="px-3 py-1 rounded-md hover:bg-zinc-100 active:bg-zinc-200">
+        Cadastrar
+      </Link>
+
+      <Link href="/signin" className="bg-black text-white px-3 py-1 rounded-md">
+        Login
+      </Link>
+    </div>
+  );
+}
