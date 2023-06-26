6dc05ee768d845ae0ffa0288a4698b45f7069a87
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-26 18:05:58 -0300

    cria utilitário para o localstorage

---
+++ frontend/src/utils/localStorage.ts
@@ -0,0 +1,11 @@
+export const get = <T>(key: string): T | null => {
+  const json = localStorage.getItem(key);
+  if (!json) return null;
+
+  const data = JSON.parse(json) as T;
+  return data;
+};
+
+export const set = <T>(key:string, value: T): void => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
