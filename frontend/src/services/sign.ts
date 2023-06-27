e8f99aae0b64e17d9c0cef9d7663aac2e64f8471
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-27 13:37:28 -0300

    adiciona serviço de signOut

---
+++ frontend/src/services/sign.ts
@@ -0,0 +1,58 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { ServiceResponse } from "@/types/ServiceResponse";
+import { SignInFields } from "@/types/Sign/SignIn";
+import { SignUpFields } from "@/types/Sign/SignUp";
+import { SignResponse, User } from "@/types/Sign/SignResponse";
+
+const signRoute = 'http://localhost:3001/accounts';
+
+const treatRequest = async <T>(
+  requestCallback: () => Promise<AxiosResponse<T>>
+): Promise<ServiceResponse<T>> => {
+  try {
+    const { data } = await requestCallback();
+
+    return { success: true, data };
+  } catch (error) {
+    let message = 'Algo inesperado ocorreu na requisição';
+    if (error instanceof AxiosError && error.response?.data) {
+      message = error.response.data.message || message;
+    }
+
+    return { success: false, data: { message } };
+  }
+}
+
+export const requestSignIn = async (
+  signInFields: SignInFields,
+): Promise<ServiceResponse<SignResponse>> => {
+  const response = await treatRequest<SignResponse>(() => {
+    return axios.post(`${signRoute}/signin`, signInFields);
+  });
+
+  return response;
+};
+
+export const requestSignUp = async (
+  signUpFields: SignUpFields,
+): Promise<ServiceResponse<SignResponse>> => {
+  const response = await treatRequest<SignResponse>(() => {
+    return axios.post(`${signRoute}/signup`, signUpFields)
+  });
+
+  return response;
+}
+
+export const requestUserData = async (
+  token: string,
+): Promise<ServiceResponse<User>> => {
+  const response = treatRequest<User>(() => {
+    return axios.get(`${signRoute}/me`, {
+      headers: {
+        'Authorization': token,
+      },
+    });
+  });
+
+  return response;
+};
