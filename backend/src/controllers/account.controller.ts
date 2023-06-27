5535d891c89ed8ebf83439187de3ad3be38bbfc3
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-27 02:42:32 -0300

    ajusta os controllers para as refatorações

--- backend/src/controllers/account.controller.ts
+++ backend/src/controllers/account.controller.ts
@@ -3,11 +3,11 @@ import accountService from "../services/account.service";
 import { mapErrorStatus } from "../utils/http";
 
 const handlePostAccount = async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-  const { status, data } = await accountService.createAccount(
-    { name, email, password },
-  );
+  const { status, data } = await accountService
+    .createAccount({ username, email, password });
+  
   if (status !== 'SUCCESS') {
     return res.status(mapErrorStatus(status)).json(data);
   }
@@ -17,7 +17,7 @@ const handlePostAccount = async (req: Request, res: Response) => {
 const handlePostSignIn = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
-  const { status, data } = await accountService.signIn(email, password);
+  const { status, data } = await accountService.signIn({ email, password });
   if (status !== 'SUCCESS') {
     return res.status(mapErrorStatus(status)).json(data);
   }
