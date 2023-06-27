413d2149647e335756503f3c892bbba0b23033a9
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-27 12:57:52 -0300

    envolve o layout com o provider do context

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,8 @@
-import { ReactNode } from 'react'
 import { Inter } from 'next/font/google'
 import { Metadata } from 'next'
 import './globals.css'
+import { ChildrenProps } from '@/types/ChildrenProps'
+import { AuthProvider } from '@/contexts/AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,15 +14,13 @@ export const metadata: Metadata = {
   description: 'Um blog comunitário onde todos podem libertar sua criatividade, escrever e compartilhar suas ideias',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode
-}) {
+export default function RootLayout({ children }: ChildrenProps) {
   return (
     <html lang="pt-br">
       <body className={`${inter.className} max-w-[1920px] m-auto min-h-screen flex flex-col`}>
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   )
