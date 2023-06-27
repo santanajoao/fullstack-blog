48dc760078d016ebee1df058e3d9ac49b0505ef4
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-27 12:07:19 -0300

    cria rota GET /accounts/me

--- backend/src/routers/account.routes.ts
+++ backend/src/routers/account.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import accountController from '../controllers/account.controller';
 import checkForSignUpFields from '../middlewares/checkForSignUpFields';
 import checkForSignInFields from '../middlewares/checkForSignInFields';
+import validateToken from '../middlewares/validateToken';
 
 const accountRouter = Router();
 
 accountRouter.post('/signup', checkForSignUpFields, accountController.handlePostAccount);
 accountRouter.post('/signin', checkForSignInFields, accountController.handlePostSignIn);
+accountRouter.get('/me', validateToken, accountController.handleGetAccountByEmail);
 
 export default accountRouter;
