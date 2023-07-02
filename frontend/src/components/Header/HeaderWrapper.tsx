4ac5db62608faf01761e2684839aa712984631e8
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-07-02 19:49:53 -0300

    corrige padding do header

--- frontend/src/components/Header/HeaderWrapper.tsx
+++ frontend/src/components/Header/HeaderWrapper.tsx
@@ -2,7 +2,7 @@ import { ChildrenProps } from "@/types/ChildrenProps";
 
 export default function HeaderWrapper({ children }: ChildrenProps) {
   return (
-    <header className="flex justify-between h-16 items-center py-2 border-b border-zinc-300 sm:px-5 sm:py-3">
+    <header className="flex justify-between h-16 items-center px-3 py-2 border-b border-zinc-300 sm:px-5 sm:py-3">
       {children}
     </header>
   );
