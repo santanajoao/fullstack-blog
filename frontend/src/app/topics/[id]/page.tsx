eed041199c1a34f4db0fd4a8b761bb9155c6aa91
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-07-07 20:56:12 -0300

    cria componente TopicHero

--- frontend/src/app/topics/[id]/page.tsx
+++ frontend/src/app/topics/[id]/page.tsx
@@ -1,8 +1,8 @@
 import HomeHeader from '@/components/Header/HomeHeader'
 import PostList from '@/components/PostList';
+import TopicHero from '@/components/TopicHero';
 import { Post } from '@/types/Post';
 import { Topic } from '@/types/Topic';
-import Image from 'next/image';
 import React from 'react'
 
 interface Params {
@@ -24,42 +24,13 @@ export default async function Topic({ params }: Params) {
     `http://backend:3001/topics/${params.id}/posts`,
     { next: { revalidate: 60 * 10 } },
   );
-  
   const data: Data = await response.json();
-  const likes = data.posts.reduce((sum, post) => sum + post.likes, 0);
   
   return (
     <>
       <HomeHeader />
       <main>
-        <header className="relative h-40 bg-cover text-white">
-          <Image
-            src={`${data.topic.imageUrl}?size=1080`}
-            width={1080}
-            height={200}
-            alt={`${data.topic.name} banner`}
-            className="z-0 absolute inset-0 h-full w-full object-cover"
-          />
-
-          <div className="z-10 relative bg-black/60 h-full w-full px-4 sm:px-6 py-10 space-y-2">
-            <h1 className="font-bold text-lg sm:text-2xl">
-              Publicações sobre:
-              &nbsp;
-              <span className="underline font-normal">{data.topic.name}</span>
-            </h1>
-
-            <div className="flex gap-4">
-              <span className="text-sm">
-                <span className="font-bold">{data.posts.length}</span> postagens
-              </span>
-              
-              <span className="text-sm">
-                <span className="font-bold">{likes}</span> likes
-              </span>
-            </div>
-          </div>
-        </header>
-
+        <TopicHero topicId={params.id} />
         <div className="px-3 sm:px-5 pt-3 pb-5">
           <div className="space-x-5 pl-1">
             <label htmlFor="sort-posts">Ordenar por:</label>
---
+++ frontend/src/components/TopicHero.tsx
@@ -0,0 +1,49 @@
+import { Topic } from "@/types/Topic";
+import Image from "next/image";
+
+type ResponseData = {
+  topic: Topic,
+  posts: {
+    likes: number,
+    quantity: number, 
+  },
+};
+
+interface Params {
+  topicId: string,
+}
+
+export default async function TopicHero({ topicId }: Params) {
+  const response = await fetch(`http://backend:3001/topics/${topicId}/posts/infos`);
+  const data = await response.json() as ResponseData;
+  
+  return (
+    <header className="relative h-40 bg-cover text-white">
+      <Image
+        src={`${data.topic.imageUrl}?size=1080`}
+        width={1080}
+        height={200}
+        alt={`${data.topic.name} banner`}
+        className="z-0 absolute inset-0 h-full w-full object-cover"
+      />
+
+      <div className="z-10 relative bg-black/60 h-full w-full px-4 sm:px-6 py-10 space-y-2">
+        <h1 className="font-bold text-lg sm:text-2xl">
+          Publicações sobre:
+          &nbsp;
+          <span className="underline font-normal">{data.topic.name}</span>
+        </h1>
+
+        <div className="flex gap-4">
+          <span className="text-sm">
+            <span className="font-bold">{data.posts.quantity}</span> postagens
+          </span>
+          
+          <span className="text-sm">
+            <span className="font-bold">{data.posts.likes}</span> likes
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+}
