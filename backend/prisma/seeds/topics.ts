4d737d647718f54384206e75c03c5ff95d061084
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-07-22 11:16:35 -0300

    refatoração da seeder: cria arquivo com os tópicos

---
+++ backend/prisma/seeds/topics.ts
@@ -0,0 +1,104 @@
+const topics = [
+  {
+    id: 'b5a2c7e0-7c5a-4a1e-bd6b-7cfe8c8a7bde',
+    name: 'Moda',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/retrato-da-moda-da-jovem-mulher-elegante_1328-2743.jpg'
+  },
+  {
+    id: 'f9fbae01-6c1d-4c9e-9dbf-85db0d6b2e0e',
+    name: 'Inteligência Artificial',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/tecnologia-de-fundo-de-toque-humano-remake-moderno-de-a-criacao-de-adam_53876-129794.jpg'
+  },
+  {
+    id: 'd3f3e6eb-fd3a-4df0-bccf-2f9a5dce7c5b',
+    name: 'Esporte',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/ferramentas-esportivas_53876-138077.jpg'
+  },
+  {
+    id: '4b6a50d1-cbfa-4e51-b6a1-b9f59fcf71a2',
+    name: 'Progamação',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/vista-traseira-do-programador-trabalhando-a-noite-toda_1098-18697.jpg'
+  },
+  {
+    id: 'c3d0be41-ebdf-4b51-a3d6-c1a7fda2b5c4',
+    name: 'Música',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/fundo-musical-volumetrico-com-uma-clave-de-sol-e-ia-geradora-de-notas_169016-29576.jpg'
+  },
+  {
+    id: '0cfbf9f3-dc24-4e12-bd60-d6a5cddd4b08',
+    name: 'Viagem',
+    imageUrl: 'https://img.freepik.com/vetores-gratis/conjunto-de-adesivos-e-crachas-de-viagem_53876-100734.jpg'
+  },
+  {
+    id: '1d2e6f24-fd27-4f0c-bd18-cb9cd2068a91',
+    name: 'Saúde',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/mao-segurando-o-conceito-de-saude-do-estetoscopio_53876-129536.jpg'
+  },
+  {
+    id: '8ed71d64-ded2-4e67-a0c1-c5c9ecf719f2',
+    name: 'Carreira',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/mulher-de-negocios-posando-com-os-bracos-cruzados-na-cidade_23-2148767057.jpg'
+  },
+  {
+    id: '9cf1e012-fed2-4ff7-b6cf-d0ddfce219e3',
+    name: 'Tecnologia',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/close-up-do-ciberespaco-do-circuito-com-luzes-de-neon_90220-1200.jpg'
+  },
+  {
+    id: '5ad2f0e8-d2df-4ae8-a5cf-e2ddfce219e3',
+    name: 'Hobbie',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/jovem-loira-pintando-com-acrilico_23-2148854525.jpg'
+  },
+  {
+    id: '6cf1e012-fed2-4ff7-b6cf-d0ddfce219e3',
+    name: 'Jogos',
+    imageUrl: 'https://img.freepik.com/psd-gratuitas/fundo-de-banner-em-branco-para-jogos_23-2150390425.jpg'
+  },
+  {
+    id: '7df1e012-fed2-4ff7-b6cf-d0ddfce219e3',
+    name: 'Educação',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/livro-com-fundo-de-placa-verde_1150-3837.jpg'
+  },
+  {
+    id: '8ef1e012-fed2-4ff7-b6cf-d0ddfce219e3',
+    name: 'Cultura',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/colorido-pintado-superficie-geometrico-formas_23-2147625971.jpg'
+  },
+  {
+    id: '9ff1e012-fed2-4ff7-b6cf-d0ddfce219e3',
+    name: 'Beleza',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/retrato-de-uma-jovem-mulher-bonita-com-escova-de-cabelo_23-2150331711.jpg'
+  },
+  {
+    id: 'a0f1e012-fed2-4ff7-b6cf-d0ddfce219e3',
+    name: 'Finanças',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/close-up-em-objetos-de-educacao-e-economia_23-2149113525.jpg'
+  },
+  {
+    id: 'b1f1e012-fed2-4ff7-b6cf-d0ddfce219e3',
+    name: 'Culinária',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/ingredientes-para-cozinhar_1220-4544.jpg'
+  },
+  {
+    id: 'c2f1e012-fed2-4ff7-b6cf-d0ddfce219e3',
+    name: 'Arte',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/pintura-abstrata-colorida_23-2147625971.jpg'
+  },
+  {
+    id: 'd3f1e012-fed2-4ff7-b6cf-d0ddfce219e3',
+    name: 'Cinema',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/um-balde-de-pipoca-e-um-copo-de-pipoca-estao-sobre-a-mesa-de-um-cinema_1340-34294.jpg'
+  },
+  {
+    id: 'e4f1e012-fed2-4ff7-b6cf-d0ddfce219e3',
+    name: 'Literatura',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/fila-de-livros-antigos-preenche-estantes-antigas-geradas-por-ia_188544-30269.jpg'
+  },
+  {
+    id: 'f5f1e012-fed2-4ff7-b6cf-d0ddfce219e3',
+    name: 'Fotografia',
+    imageUrl: 'https://img.freepik.com/fotos-gratis/camera-profissional-em-um-desfocado_169016-10249.jpg'
+  },
+];
+
+export default topics;
