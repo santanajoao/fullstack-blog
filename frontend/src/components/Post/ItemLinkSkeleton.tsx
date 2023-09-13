2d867d866a7ebb0202839f3cd284b22db666752c
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-09-13 15:16:20 -0300

    remove estilização desnecessária do skeleton

--- frontend/src/components/Post/ItemLinkSkeleton.tsx
+++ frontend/src/components/Post/ItemLinkSkeleton.tsx
@@ -4,11 +4,11 @@ export default function ItemLinkSkeleton() {
   return (
     <div className="h-full w-full flex flex-col gap-1 pt-2 px-1 pb-3">
       <div
-        className="object-cover rounded-2xl w-full aspect-video bg-primaryGreen bg-opacity-50"
+        className="rounded-2xl w-full aspect-video bg-primaryGreen bg-opacity-50"
       />
-      <div className="text-xs mt-1 h-2 bg-gray-300" />
-      <div className="font-bold text-lg leading-6 line-clamp-3 break-words bg-gray-300 h-6" />
-      <div className="text-justify line-clamp-4 break-words leading-5 bg-gray-300 h-20" />
+      <div className="mt-1 h-2 bg-gray-300" />
+      <div className="bg-gray-300 h-6" />
+      <div className="bg-gray-300 h-20" />
     </div>
   );
 }
