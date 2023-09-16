79d6c6cd557e0487371d23d27597886883833798
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-09-16 15:15:43 -0300

    refatora validações da entidade like para utilizar a camada model

--- backend/src/services/validations/likeValidations.ts
+++ backend/src/services/validations/likeValidations.ts
@@ -1,15 +1,12 @@
 import { Likes } from '@prisma/client';
 import prisma from '../../lib/prisma';
 import { AsyncServiceResponse } from '../../types/serviceResponse';
+import * as likeModel from '../../models/like.model';
 
 export const checkForLike = async (
   accountId: string, postId: string
 ): AsyncServiceResponse<Likes> => {
-  const like = await prisma.likes.findUnique({
-    where: {
-      accountId_postId: { accountId, postId },
-    },
-  });
+  const like = await likeModel.findLikeByIds(accountId, postId);
 
   if (!like) {
     return { status: 'NOT_FOUND', data: { message: 'Você não deu like nesse post' } }
