2f2111c671d01b64be2e261b6c44a816b145d950
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-09-16 16:57:43 -0300

    remove validação do formato do id

--- backend/src/services/validations/schemas/uuid.schema.ts
+++
@@ -1,4 +0,0 @@
-import z from 'zod';
-
-export const uuidLength = 36;
-export const uuidSchema = z.string().length(uuidLength);
--- backend/src/services/validations/validateTopicId.ts
+++ backend/src/services/validations/validateTopicId.ts
@@ -1,14 +1,8 @@
 import { Topic } from '@prisma/client';
 import { AsyncServiceResponse } from '../../types/serviceResponse';
-import { uuidSchema } from './schemas/uuid.schema';
 import * as topicModel from '../../models/topic.model';
 
 const validateTopicId = async (uuid: string): AsyncServiceResponse<Topic> => {
-  const lengthValidation = uuidSchema.safeParse(uuid);
-  if (!lengthValidation.success) {
-    return { status: 'INVALID_VALUE', data: { message: 'id inválido' } };
-  }
-
   const topic = await topicModel.findTopicById(uuid);
 
   if (!topic) {
