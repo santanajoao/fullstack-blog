152ab06e7ce73539971c120c73fb6873c91619a0
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-09-16 16:52:24 -0300

    refatora service da entidade topic para utilizar a camda model

--- backend/src/services/topic.service.ts
+++ backend/src/services/topic.service.ts
@@ -1,62 +1,25 @@
 import { Topic } from '@prisma/client';
 import { AsyncServiceResponse } from '../types/serviceResponse';
-import prisma from '../lib/prisma';
-import treatQuantity from './validations/treatQuantity';
-import { getDateDaysAgo } from '../utils/dates';
+import * as topicModel from '../models/topic.model';
 
 const getWeekPopularTopics = async (
   quantity: number
 ): AsyncServiceResponse<Topic[]> => {
-  const topics = await prisma.topic.findMany({
-    where: {
-      posts: {
-        some: {
-          createdAt: {
-            gte: getDateDaysAgo(7),
-          },
-        },
-      },
-    },
-    take: treatQuantity(quantity),
-    orderBy: {
-      posts: {
-        _count: 'desc',
-      },
-    },
-  });
+  const topics = await topicModel.findWeekPopularTopics(quantity);
 
   return { status: 'SUCCESS', data: topics };
 };
 
 const getAccountTopics = async (
-  accountId: string,
+  id: string,
 ): AsyncServiceResponse<Topic[]> => {
-  const topics = await prisma.topic.findMany({
-    where: {
-      posts: {
-        some: {
-          accountId,
-        },
-      },
-    },
-    distinct: ['id'],
-  });
+  const topics = await topicModel.findTopicsByAccoundId(id);
 
   return { status: 'SUCCESS', data: topics };
 };
 
 const getTopics = async (query: string = '', excludeIds: string[] = []): AsyncServiceResponse<Topic[]> => {
-  const topics = await prisma.topic.findMany({
-    where: {
-      name: {
-        contains: query,
-      },
-      id: {
-        notIn: excludeIds,
-      },
-    },
-    take: 6,
-  });
+  const topics = await topicModel.findTopicsByTopicName(query, excludeIds);
 
   return { status: 'SUCCESS', data: topics };
 };
