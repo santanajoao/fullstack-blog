993569ed5dbfcfa701e6538bc13fdf30f5cbd4ea
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-10-04 19:32:55 -0300

    adiciona validações no formulário de comentar

--- frontend/src/app/post/[id]/page.tsx
+++ frontend/src/app/post/[id]/page.tsx
@@ -4,10 +4,7 @@ import Footer from '@/components/Footer';
 import Post from '@/components/Post';
 import Container from '@/components/Container';
 import { requestPostById } from '@/services/posts';
-import CommentForm from '@/components/Post/CommentForm';
-import CommentCard from '@/components/Post/CommentCard';
-import profilePicture from '@/assets/profile.svg';
-import Button from '@/components/PostListPagination/Button';
+import CommentSection from '@/components/Post/CommentSection';
 
 interface Params {
   params: {
@@ -39,35 +36,7 @@ export default async function PostPage({ params }: Params) {
           </Container.Markdown>
         </section>
 
-        <section className="mt-10">
-          <header className="flex items-center p-1 border-t-2 border-black/10 justify-between">
-            <h2 className="text-xl font-bold">Comentários</h2>
-            <span className="font-medium text-sm">14 Comentários</span>
-          </header>
-
-          <CommentForm />
-
-          <ul className="mt-4 flex flex-col gap-2">
-            <li>
-              <CommentCard
-                username="Maria Carla"
-                profilePicture={profilePicture}
-                comment="Que post legal! Continue postando."
-              />
-            </li>
-            <li>
-              <CommentCard
-                username="Zeca Pagodinho"
-                profilePicture={profilePicture}
-                comment="Agora eu começo a fazer minhas publicações!"
-              />
-            </li>
-          </ul>
-
-          <Button type="button">
-            Ver mais comentários
-          </Button>
-        </section>
+        <CommentSection />
       </main>
       <Footer />
     </>
--- frontend/src/components/Post/CommentForm/index.tsx
+++ frontend/src/components/Post/CommentForm/index.tsx
@@ -1,15 +1,41 @@
+'use client';
+
 import Textarea from '@/components/Profile/Textarea';
 import React from 'react';
 import Sign from '@/components/Sign';
-import { UseFormRegister } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { commentFieldsSchema } from '@/lib/schemas/comment.schema';
+
+type CommentFields = {
+  comment: string;
+};
 
 export default function CommentForm() {
-  const register = function () {} as unknown as UseFormRegister<any>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CommentFields>({
+    resolver: zodResolver(commentFieldsSchema),
+  });
+
+  const onSubmit = (fields: CommentFields): void => {
+    console.log(fields);
+  };
 
   return (
-    <form>
-      <Textarea placeholder="Que post legal!" register={register} name="" />
-      <Sign.Button className="py-2" type="button">Comentar</Sign.Button>
+    <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+      <Textarea
+        aria-label="Deixe seu comentário"
+        placeholder="Deixe aqui seu comentário"
+        name="comment"
+        register={register}
+      />
+
+      <Sign.ErrorMessage>{errors.comment?.message}</Sign.ErrorMessage>
+
+      <Sign.Button className="py-2 w-fit mt-1" type="submit">Comentar</Sign.Button>
     </form>
   );
 }
