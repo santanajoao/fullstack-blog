69767384f5b514fd28a1c87194687cd89e153b6a
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-12-22 14:53:52 -0300

    feat: add a link to topic on topic list

--- frontend/src/components/SimpleTopicList/index.tsx
+++ frontend/src/components/SimpleTopicList/index.tsx
@@ -1,4 +1,5 @@
 import { TopicWithoutImage } from '@/types/Topic';
+import Link from 'next/link';
 import React from 'react';
 
 interface Props {
@@ -9,8 +10,10 @@ export default function SimpleTopicList({ topics }: Props) {
   return (
     <ul className="flex flex-wrap gap-1">
       {topics.map((topic) => (
-        <li key={topic.id} className="text-sm font-medium px-2 py-1 rounded-md bg-gray-300">
-          {topic.name}
+        <li key={topic.id}>
+          <Link href={`/topic/${topic.id}`} className="text-sm font-medium px-2 py-1 rounded-md bg-gray-300">
+            {topic.name}
+          </Link>
         </li>
       ))}
     </ul>
